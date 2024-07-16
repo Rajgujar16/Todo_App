95fea1eed9ef8f92b66e7373f376d57967394032
Rajgujar16 <153699836+Rajgujar16@users.noreply.github.com>
2024-07-16 21:39:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@
             navbar.style.backgroundColor = ""; // Reset inline background color for navbar
             themeBtn.innerText = "Dark";
             h1.style.color = "black";
-            span.style.color = "dark"
+            span.style.color = "black"
             theme = 0;
         }
     });
